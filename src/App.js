735e8204a2ce4b150d8f94b54e994155d7d473b8
krishna0999 <krishnamuraree123@gmail.com>
2022-07-20 08:52:59 +0530

    fix: finsihed the home page for search-news

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const handleSearchQuery = (e) => {
+    fetch(`http://hn.algolia.com/api/v1/search?query=${e.target.value}`)
+      .then((response) => response.json())
+      .then((data) => setSearchResults(data.hits));
+    console.log(searchResults);
+  };
+
+  const someFn = function (fn, delay) {
+    let timer;
+    return function () {
+      let context = this,
+        args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        handleSearchQuery.apply(context, args);
+      }, delay);
+    };
+  };
+  const optimisedSearchFunction = someFn(handleSearchQuery, 300);
+
+  const handleNavigateDetailPage = (e) => {
+    console.log(e);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__search">
+        <h1>Search across Hacker news</h1>
+        <input
+          type="text"
+          onKeyUp={optimisedSearchFunction}
+          placeholder="Search here..."
+        />
+      </div>
+      <div className="app__searchResults">
+        {searchResults?.map((result) => (
+          <div className="individual_result">
+            <p onClick={handleNavigateDetailPage}>
+              {result.title || result.story_text}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
+// http://hn.algolia.com/api/v1/search?query=...
 export default App;
